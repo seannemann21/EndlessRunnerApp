08e54ebe478b2386923ad1d9dd2975a83148926c
Sean Nemann <seannemann21@gmail.com>
2017-11-09 16:27:15 -0500

    Lowered Framerate of game

--- Runner/build/generated/source/r/androidTest/debug/com/example/endlessrunnerapp/game/R.java
+++ Runner/build/generated/source/r/androidTest/debug/com/example/endlessrunnerapp/game/R.java
@@ -8,11 +8,11 @@ package com.example.endlessrunnerapp.game;
 
 public final class R {
     public static final class drawable {
-        public static final int app_banner = 0x7f020000;
-        public static final int app_icon = 0x7f020001;
+        public static final int app_banner = 0x7f010000;
+        public static final int app_icon = 0x7f010001;
     }
     public static final class string {
-        public static final int app_name = 0x7f040000;
+        public static final int app_name = 0x7f020000;
     }
     public static final class style {
         public static final int UnityThemeSelector = 0x7f030000;
